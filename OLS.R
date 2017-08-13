3888fa046cae4feda698f4cce2e38e541ee7f9b6
azzouz marouen <azzouz.marouen@gmail.com>
2017-08-13 18:28:42 +0200

    Update OLS.R
    Ordinary least squares

--- OLS.R
+++ OLS.R
@@ -10,6 +10,7 @@ LR<-function(input,response){
   var_beta=solve(t(X)%*%X)*sigma2
   return(list(yhat=yhat,beta=beta,sigma2=sigma2,varBeta=var_beta))
 }
+
 #predictions for any linear model
 predictLS<-function(LSModel=NULL,X,beta=NULL){
   if(!is.null(LSModel)){beta=LSModel$beta}
